20524865f3100667bee7f64146db523e57b5af05
Yahya Zini <yahyazini@gmail.com>
2017-07-28 16:27:51 -0400

    Update the init to get the latest version of Laravel

--- commands/init-app.sh
+++ commands/init-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+git clone https://github.com/laravel/laravel.git web/src
 docker-compose run web bash <<'EOF'
+cp .env.example .env
 composer install
 php artisan key:generate
 chgrp -R www-data storage bootstrap/cache
