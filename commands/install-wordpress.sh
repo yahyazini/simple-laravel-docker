e4ddfc3662a74aa48a5e3cd77362e6d418ef6640
Yahya Zini <yahyazini@gmail.com>
2017-07-28 17:18:26 -0400

    Adding wp command and database

--- commands/install-wordpress.sh
+++ commands/install-wordpress.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+docker-compose run mysql bash <<'EOF'
+echo 'CREATING WORDPRESS DB'
+mysql -h $LOCALHOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress";
+echo 'DONE!'
+EOF
+
 docker-compose run web bash <<'EOF'
+echo 'INSTALLING WORDPRESS'
 sudo rm -rf public/wordpress
 mkdir public/wordpress
 cd public/wordpress
@@ -10,3 +18,4 @@ tar  -xz --strip-components=1 -f wordpress.tar.gz \
 		&& chown -R www-data:www-data wp-content
 echo 'DONE!'
 EOF
+docker cp ./web/config/wp/wp-config.php web:/var/www/html/public/wordpress
